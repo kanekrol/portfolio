46813e93d8251b5cef029007acdaa677ab49a4c0
kanekrol <113343216+kanekrol@users.noreply.github.com>
2023-09-14 14:01:33 +0200

    projectseeders aangemaakt

---
+++ database/seeders/ProjectsSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProjectsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Project::create([
+            'title' => 'Project 1',
+            'description' => 'Dit is project 1',
+            'image' => 'img/project1.jpg'
+        ]);
+        Project::create([
+            'title' => 'Project 2',
+            'description' => 'Dit is project 2',
+            'image' => 'img/project1.jpg'
+        ]);
+        Project::create([
+            'title' => 'Project 3',
+            'description' => 'Dit is project 3',
+            'image' => 'img/project1.jpg'
+        ]);
+    }
+}
