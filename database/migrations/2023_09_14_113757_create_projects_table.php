328da6357023f9bd6edb3c058e903288b860acbf
kanekrol <113343216+kanekrol@users.noreply.github.com>
2023-09-14 13:47:36 +0200

    description niet nullable gemaakt

--- database/migrations/2023_09_14_113757_create_projects_table.php
+++ database/migrations/2023_09_14_113757_create_projects_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->binary('image');
-            $table->text('description')->nullable();
+            $table->text('description');
             $table->timestamps();
         });
     }
