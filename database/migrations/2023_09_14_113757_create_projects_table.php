fa8de630cc9ec2f1f08e9451110fd4be4c2d7073
kanekrol <113343216+kanekrol@users.noreply.github.com>
2023-09-14 13:42:31 +0200

    migration tabellen toegevoegd

--- database/migrations/2023_09_14_113757_create_projects_table.php
+++ database/migrations/2023_09_14_113757_create_projects_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->binary('image');
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
