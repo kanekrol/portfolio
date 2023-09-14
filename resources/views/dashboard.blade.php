5efef57b4d55cc28dc068a7e446d1838c4b071b6
kanekrol <113343216+kanekrol@users.noreply.github.com>
2023-09-14 14:14:02 +0200

    tabellen voor projects in dashboard aangemaakt

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,24 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Titel</th>
+                                <th>Beschrijving</th>
+                                <th>Afbeelding</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($projects as $project)
+                            <tr>
+                                <td>{{$project->title}}</td>
+                                <td>{{$project->description}}</td>
+                                <td>{{$project->image}}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
