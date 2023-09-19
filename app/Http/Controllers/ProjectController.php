0f04298d86c2b8018d8aa47e3b2832c3fd39d7b2
kanekrol <113343216+kanekrol@users.noreply.github.com>
2023-09-19 23:50:42 +0200

    edit en delete werkend gekregen in de controller

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -62,24 +62,58 @@ class ProjectController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit($id)
     {
-        //
+        $project = Project::findOrFail($id); // Zoek het project op basis van het ID
+        return view('projects.edit', compact('project')); // Laad de bewerkingsweergave en geef het project door
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'title' => ['required', 'max:255'],
+            'image' => ['image', 'max:2048'], // Validatieregels voor afbeeldingen bijwerken
+            'description' => ['required'],
+        ]);
+
+        $project = Project::findOrFail($id); // Zoek het project op basis van het ID
+
+        // Bijwerken van de projectgegevens
+        $project->title = $request->input('title');
+        
+        // Controleer of er een nieuwe afbeelding is geüpload en update de afbeelding indien nodig
+        if ($request->hasFile('image')) {
+            // Verwijder de oude afbeelding (optioneel)
+            // Storage::delete('public/' . $project->image);
+
+            // Upload de nieuwe afbeelding en bewaar het pad
+            $imagePath = $request->file('image')->store('project_images', 'public');
+            $project->image = $imagePath;
+        }
+
+        $project->description = $request->input('description');
+        $project->save();
+
+        return redirect()
+            ->route('dashboard')
+            ->with('success', 'Project bijgewerkt');
     }
+
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $project = Project::findOrFail($id); // Zoek het project op basis van het ID
+
+        // Optioneel: Verwijder de bijbehorende afbeelding als die er is
+        // Storage::delete('public/' . $project->image);
+
+        $project->delete();
+
+        return redirect()
+            ->route('dashboard')
+            ->with('success', 'Project verwijderd');
     }
 }
