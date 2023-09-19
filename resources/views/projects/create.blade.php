460afc1e34410ea46167f43436e870a9822f85b2
kanekrol <113343216+kanekrol@users.noreply.github.com>
2023-09-19 23:40:03 +0200

    create form aangemaakt

---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,44 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Project aanmaken') }}
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    <form method="POST" action="{{ route('projects.store') }}" enctype="multipart/form-data">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="title" class="form-label">Titel</label>
+                            <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}">
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="image" class="form-label">Afbeelding</label>
+                            <input type="file" class="form-control" id="image" name="image">
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="description" class="form-label">Beschrijving</label>
+                            <textarea class="form-control" id="description" name="description">{{ old('description') }}</textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Opslaan</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
