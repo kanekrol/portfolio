b44b4ac1d741def5c0c3f55d56eab545ee72adfd
kanekrol <113343216+kanekrol@users.noreply.github.com>
2023-09-21 21:11:53 +0200

    category seeder aangemaakt

---
+++ database/seeders/CategoriesSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategoriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Category::create([
+            'name' => 'Laravel',
+        ]);
+
+        Category::create([
+            'name' => 'Laravel2',
+        ]);
+
+        Category::create([
+            'name' => 'Laravel3',
+        ]);
+    }
+}
