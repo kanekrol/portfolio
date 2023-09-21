87a2c935312389f6f971501685925cd599174ae5
kanekrol <113343216+kanekrol@users.noreply.github.com>
2023-09-21 21:12:03 +0200

    database seeder gevuld

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(ProjectsSeeder::class);
+        $this->call(CategoriesSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
