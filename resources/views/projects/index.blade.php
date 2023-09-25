6449ba54740d909e5c6caffb703d7ae6a1515991
kanekrol <113343216+kanekrol@users.noreply.github.com>
2023-09-25 14:06:26 +0200

    search button toegevoegd aan publieke projecten pagina

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,10 +1,22 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
 <x-app-layout>
-
+    <x-slot name="header">
+        <div class="pull-right mb-2">
+            <h1>Mijn projecten</h1>
+        </div>
+    </x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
+                    <div class="col-md-6">
+                        <form method="get" action="/search">
+                            <div class="input-group">
+                                <input class="form-control" name="search" placeholder="Search..." value="{{ isset($search) ? $search : ''}}">
+                                <button type="submit" class="btn btn-primary">Zoek</button>
+                            </div>
+                        </form>
+                    </div>
                     <table class="table table-bordered">
                         <thead>
                             <tr>
